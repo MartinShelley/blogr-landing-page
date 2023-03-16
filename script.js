e36b550aa74aeecae5da75b1e08b99da48edc5c7
MartinShelley <martin.shelley91@gmail.com>
2023-03-16 15:46:48 +0000

    built mobile nav

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 (function () {
   const hamburgerIcon = document.querySelector('.hamburger-icon');
+  const mobileNav = document.querySelector('.mobile-nav');
 
-  hamburgerIcon.addEventListener('click', function () {
-    document.querySelector('.mobile-nav').style.display = "block";
+  hamburgerIcon.addEventListener('click', function (e) {
+    if (mobileNav.style.display === "") {
+      mobileNav.style.display = "block";
+    }
+    else {
+      mobileNav.style.display = "";
+    }
+  });
+
+  document.querySelectorAll('.nav-option').forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      console.log(e.currentTarget);
+      if (!e.currentTarget.classList.contains('nav-option-selected')) {
+        console.log("open");
+        e.currentTarget.classList.add('nav-option-selected');
+        e.currentTarget.children[0].children[0].classList.add('rotate');
+      }
+      else {
+        console.log("close");
+        e.currentTarget.classList.remove('nav-option-selected');
+        e.currentTarget.children[0].children[0].classList.remove('rotate');
+      }
+    });
   });
 })();
